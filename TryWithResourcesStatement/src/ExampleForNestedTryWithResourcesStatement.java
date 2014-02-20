47cbaca758eddcee0a3623ec4b3d4cc79992714b
Pig <pig211@hotmail.com>
2014-02-20 11:30:44 +0800

    * add some comment of an example

--- TryWithResourcesStatement/src/ExampleForNestedTryWithResourcesStatement.java
+++ TryWithResourcesStatement/src/ExampleForNestedTryWithResourcesStatement.java
@@ -1,5 +1,8 @@
 public class ExampleForNestedTryWithResourcesStatement {
 
+	/*
+	 * Can't compile, just an example to show the people in c# will code like this
+	 */
 	public static void nestedTryWhenUsingConnection() {
 		try (Connection connection = new Connection()) {
 			connection.doSomeSetUp();
